52ecae8aae8e3ebbd03f759b78198e36316193ff
Matthias Bock <mail@matthiasbock.net>
2018-06-09 12:01:51 +0200

    Tell about unreachable fluorometer

--- publisher.sh
+++ publisher.sh
@@ -3,6 +3,13 @@
 topic="fluorometer/value/raw"
 
 while [ 1 ]; do
+	ping=$(ping 192.168.4.1 -c 1 | fgrep "1 received" 2>&1)
+	if [ "$ping" == "" ]; then
+		echo "Fluorometer not reachable"
+		sleep 1
+		continue
+	fi
+
 	value=$(./read.sh)
 	echo "Fluorometer value: $value"
 	mosquitto_pub -t $topic -m $value
